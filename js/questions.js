c2e7ce7be889ee4c941d2e00d16ecf13df9f26db
arpa2001 <arpajitofficial@gmail.com>
2022-01-21 10:50:29 +0530

    Updated
    Added the assignment

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is correct about JavaScript?",
+    answer: "JavaScript is an Object-Based language",
+    options: [
+      "JavaScript is an Object-Based language",
+      "JavaScript is Assembly-language",
+      "JavaScript is an Object-Oriented language",
+      "JavaScript is a High-level language"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Arrays in JavaScript are defined by which of the following statements?",
+    answer: "It is an ordered list of values",
+    options: [
+      "It is an ordered list of values",
+      "It is an ordered list of string",
+      "It is an ordered list of functions",
+      "It is an ordered list of values"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+];
